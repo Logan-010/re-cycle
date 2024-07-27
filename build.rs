ea4d60ce123621b0ca47fcc7e1bb210c10308ee7
seedse <loganbreezestl@gmail.com>
2024-07-27 17:09:32 -0500

    Linux build fix

--- build.rs
+++ build.rs
@@ -5,11 +5,14 @@ use {
 };
 
 fn main() -> io::Result<()> {
-    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        WindowsResource::new()
-            // This path can be absolute, or relative to your crate root.
-            .set_icon("./icon.ico")
-            .compile()?;
+    #[cfg(target_os = "windows")]
+    {
+        if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+            WindowsResource::new()
+                // This path can be absolute, or relative to your crate root.
+                .set_icon("./icon.ico")
+                .compile()?;
+        }
     }
     Ok(())
 }
